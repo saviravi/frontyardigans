6267315618192b3a24586e4536c10f582908c447
jacurick19 <34878168+jacurick19@users.noreply.github.com>
2023-01-31 18:51:48 -0500

    Skelaton code

--- Recommendation/Recommendation.py
+++ Recommendation/Recommendation.py
@@ -3,3 +3,10 @@
 
 def handleInput(inputData):
     return "This connection works"
+
+
+# Get locations with duffel
+
+# Then get resturants and weather
+
+# pick the one that matches the user's input the best
