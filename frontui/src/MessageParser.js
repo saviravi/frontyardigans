ce9481258cf36f62a68ffa3893d21beb1daafc2a
Keyang Zhang <jxzhangkeyang@icloud.com>
2023-02-07 18:36:27 -0500

    updated three js files for frontend

--- frontui/src/MessageParser.js
+++ frontui/src/MessageParser.js
@@ -1,12 +1,20 @@
-class MessageParser {
-    constructor(actionProvider, state) {
-      this.actionProvider = actionProvider;
-      this.state = state;
-    }
-  
-    parse(message) {
-      console.log(message)
-    }
-  }
+import React from 'react';
+
+const MessageParser = ({ children, actions }) => {
+  const parse = (message) => {
+    console.log(message);
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          parse: parse,
+          actions: {},
+        });
+      })}
+    </div>
+  );
+};
 
 export default MessageParser;
