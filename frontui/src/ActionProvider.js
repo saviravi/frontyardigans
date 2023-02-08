747141f7f037fc878d467b2a3e19048ea4fe6a8f
Kevin <k_le@outlook.com>
2023-02-07 20:36:12 -0500

    connected Rasa with frontend

--- frontui/src/ActionProvider.js
+++ frontui/src/ActionProvider.js
@@ -1,19 +1,33 @@
-class ActionProvider {
-    constructor(
-     createChatBotMessage,
-     setStateFunc,
-     createClientMessage,
-     stateRef,
-     createCustomMessage,
-     ...rest
-   ) {
-     this.createChatBotMessage = createChatBotMessage;
-     this.setState = setStateFunc;
-     this.createClientMessage = createClientMessage;
-     this.stateRef = stateRef;
-     this.createCustomMessage = createCustomMessage;
-   }
- }
- 
- export default ActionProvider;
- 
+// in ActionProvider.jsx
+import React from 'react';
+
+const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const handleMessage = (data) => {
+    /*
+      Takes the response from Rasa and outputs a message box.
+      Called by Message Parser
+    */
+    const botMessage = createChatBotMessage(data[0].text);
+
+    // TODO: check if allow photos
+
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+            handleMessage,
+          },
+        });
+      })}
+    </div>
+  );
+};
+
+export default ActionProvider;
--- frontui/src/MessageParser.js
+++ frontui/src/MessageParser.js
@@ -1,12 +1,32 @@
-class MessageParser {
-    constructor(actionProvider, state) {
-      this.actionProvider = actionProvider;
-      this.state = state;
-    }
-  
-    parse(message) {
-      console.log(message)
-    }
-  }
+import React from 'react';
+import axios from 'axios';
+
+const MessageParser = ({ children, actions }) => {
+
+  /*
+    Sends post request to rasa with user input as payload.
+  */
+  const parse = (message) => {
+    axios.post('http://localhost:5005/webhooks/rest/webhook', {
+      sender: "User", // This is supposed to be a recipient ID, this is all local tho so whatever
+      message: message
+    }).then(response => {
+        actions.handleMessage(response.data);
+      }
+    );
+    
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          parse: parse,
+          actions: {},
+        });
+      })}
+    </div>
+  );
+};
 
 export default MessageParser;
