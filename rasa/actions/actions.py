c9ef6626ca07fcbcbcf3ea5ec5e7122e9507815e
jayasaal2 <jayasaal.2@buckeyemail.osu.edu>
2023-02-09 17:47:43 -0500

    Created a simple form

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -25,3 +25,26 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionSayTemperature(Action):
+
+    def name(self) -> Text:
+        return "action_say_temperature"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        temperature = tracker.get_slot("temperature")
+        if not temperature:
+            dispatcher.utter_message(text="I don't know your temp preferrence.")
+        else:
+            dispatcher.utter_message(text=f"You prefer {temperature} weather!")
+        return []
