e397ddda7e8abf3f9e9f13f8b4a7289afa72d92b
Keyang Zhang <zhang.10448@osu.edu>
2023-02-10 11:34:13 -0500

    fix minor syntax

--- frontui/src/bot/config.js
+++ frontui/src/bot/config.js
@@ -3,13 +3,13 @@ import { createChatBotMessage } from 'react-chatbot-kit';
 import BotAvatar from './BotAvatar';
 import UserAvatar from './UserAvatar';
 
-const botName = "Frontyadigans recommendation bot"
+const botName = "Frontyadigans recommendation bot";
 
 const config = {
   initialMessages: [createChatBotMessage(`Hello there, I'm ${botName}!`)],
   botName: botName,
   customComponents: {
-    botAvatar: (props) => <BotAvatar {...props}/>,
+    botAvatar: (props) => <BotAvatar {...props} />,
     userAvatar: (props) => <UserAvatar {...props} />,
   },
 };
