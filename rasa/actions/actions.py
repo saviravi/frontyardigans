6bce06e2ff311e3ba742a960cb70fddd8bece121
jayasaal2 <jayasaal.2@buckeyemail.osu.edu>
2023-02-13 23:41:28 -0500

    Simple form asking for weather and city

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -8,8 +8,8 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
+from rasa_sdk.types import DomainDict
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 import sys
 import os
@@ -22,6 +22,8 @@ print("--------------")
 sys.path.append(os.path.realpath(__file__)[:len(os.path.realpath(__file__)) - len("rasa\\actions\\actions.py")] + "Recommendation")
 import Recommendation
 
+ALLOWED_TEMP = ["hot","cold"]
+
 
 class ActionGetRecommendation(Action):
 
@@ -32,3 +34,41 @@ class ActionGetRecommendation(Action):
          domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          dispatcher.utter_message(text=Recommendation.handleInput([]))
          return []
+
+
+
+class ValidateTravelForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_travel_form"
+    def validate_temperature(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `temperature` value."""
+
+        if slot_value.lower() not in ALLOWED_TEMP:
+            dispatcher.utter_message(text=f"We only accept temps: hot/cold")
+            return {"temperature": None}
+        temperature = tracker.get_slot("temperature")
+        dispatcher.utter_message(text=f"OK! You prefer {temperature} weather.")
+        return {"temperature": slot_value}
+    
+    def validate_city(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `city` value."""
+
+        city = tracker.get_slot("city")
+        if not city:
+            dispatcher.utter_message(text=f"No city was entered")
+            return {"city": None}
+        dispatcher.utter_message(text=f"OK! You liked visiting {city}.")
+        return {"city": slot_value}
+    
