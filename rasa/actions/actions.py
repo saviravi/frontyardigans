24f02f18abf3a832375dbadd25bacb602f70bf54
jacurick19 <34878168+jacurick19@users.noreply.github.com>
2023-02-14 17:24:14 -0500

    Actually fixed

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,25 +7,19 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
- from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
- from rasa_sdk import Action, Tracker
- from rasa_sdk.executor import CollectingDispatcher
 
+class ActionHelloWorld(Action):
 
- class ActionHelloWorld(Action):
+    def name(self) -> Text:
+        return "action_hello_world"
 
-     def name(self) -> Text:
-         return "action_hello_world"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="Hello World!")
-
-         return []
-
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Hello World!")
+        return []
 
 from typing import Any, Text, Dict, List
 from rasa_sdk.types import DomainDict
@@ -34,7 +28,6 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-# import Recommendation
 
 class ActionSayTemperature(Action):
 
