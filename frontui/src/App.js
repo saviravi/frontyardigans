5fd7ead1e9381f5e2633c9f1d6764fd967472e34
Keyang Zhang <112275918+Mutsuki0110@users.noreply.github.com>
2023-03-31 14:19:15 -0400

    chatbot code structure change and about page

--- frontui/src/App.js
+++ frontui/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Navbar from './navbar/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './navbar/Navbar';
 import TravisBot from "./bot/Chatbot";
 import Highlights from "./pages/highlights";
-import About from './pages/about'
+import About from './pages/about';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
 
 function App() {
   return(
     <Router>
       <Navbar sticky="top" />
       <Routes>
-        <Route exact path='/' element={<TravisBot />} />
-        <Route exact path='/home' element={<TravisBot />} />
+        <Route path='/' element={<TravisBot />} />
+        <Route path='/home' element={<TravisBot />} />
         <Route path='/highlights' element={<Highlights />} />
         <Route path='/about' element={<About />} />
       </Routes>
--- frontui/src/bot/Chatbot.js
+++ frontui/src/bot/Chatbot.js
@@ -1,17 +1,34 @@
+import React, { useState } from 'react';
 import Chatbot from "react-chatbot-kit";
-import 'react-chatbot-kit/build/main.css'
+import 'react-chatbot-kit/build/main.css';
 import config from './config.js';
 import MessageParser from './MessageParser.js';
 import ActionProvider from './ActionProvider.js';
 
 const TravisBot = () => {
+  const [showBot, toggleBot] = useState(false);
+
+  const saveMessages = (messages, HTMLString) => {
+    localStorage.setItem('chat_messages', JSON.stringify(messages));
+  };
+
+  const loadMessages = () => {
+    const messages = JSON.parse(localStorage.getItem('chat_messages'));
+    return messages;
+  };
+
   return (
     <div>
+      {showBot && (
         <Chatbot
-            config={config}
-            messageParser={MessageParser}
-            actionProvider={ActionProvider}
+          config={config}
+          actionProvider={ActionProvider}
+          messageHistory={loadMessages()}
+          messageParser={MessageParser}
+          saveMessages={saveMessages}
         />
+      )}
+      <button onClick={() => toggleBot((prev) => !prev)}>Bot</button>
     </div>
   );
 };
